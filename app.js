88a2e1185aa3973b337d4ef34190cc8521937f1c
dori197 <xayda.ln@gmail.com>
2025-04-25 20:37:49 +0700

    Your messsage

---
+++ app.js
@@ -0,0 +1,26 @@
+particlesJS("particles-js", {
+    particles: {
+      number: {
+        value: 40,
+        density: { enable: true, value_area: 800 },
+      },
+      color: { value: "#455496" },
+      shape: { type: "triangle" },
+      opacity: { value: 0.5, random: true },
+      size: { value: 3, random: true },
+      line_linked: {
+        enable: true,
+        distance: 100,
+        color: "#3e1c54",
+        opacity: 0.2,
+        width: 1,
+      },
+      move: {
+        enable: true,
+        speed: 5,
+        direction: "none",
+        random: true,
+        out_mode: "out",
+      },
+    },
+  });
